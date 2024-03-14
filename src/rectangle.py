d3c0ed87819d05c218015f40284cf35511bffd1b
fuenukkusu2501 <fenixxx2501@gmail.com>
2024-03-14 21:12:21 +0300

    DZ for pull request

---
+++ src/rectangle.py
@@ -0,0 +1,11 @@
+class Rectriangle(input()):
+
+    def __init__(self, side_a, side_b):
+        self.side_a = side_a
+    def get_area(s1, s2, sep)
+    def get_perimeter()
+
+    def add_area(figure)
+
+def get_area(s1, s2, sep):
+    print(s1 * sep * s2)
---
+++ src/triangle.py
@@ -0,0 +1,7 @@
+class Triangle:
+
+
+    def get_area()
+    def get_perimeter()
+
+    def add_area(figure)
